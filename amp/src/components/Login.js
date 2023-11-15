5bc33ee6f01f3e10f7eb4defef36d4abf0675c3c
Joe Braha <jb7044@nyu.edu>
2023-11-15 15:48:59 -0500

    starting code for api connection

--- amp/src/components/Login.js
+++ amp/src/components/Login.js
@@ -2,26 +2,43 @@ import React, { useState } from "react";
 // importing Link from react-router-dom to navigate to  
 // different end points. 
 import { Link } from "react-router-dom"; 
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import api from './api'
 import "../App.css";
   
 
 
 //Todo: fill in with contact information
 const Login = () => { 
+  const [username, setUser] = useState("");
+  const [password, setPass] = useState("");
+  const result = {};
+
+  const formSubmit = async (event) => {
+    event.preventDefault();
+    result = await api.post('/login', {username, password});
+    setUser("");
+    setPass("");
+    console.log(result);
+  }
+  const handleChangeUser = (event) => {
+    setUser(event.target.value);
+  }
+  const handleChangePass = (event) => {
+    setPass(event.target.value);
+  }
+
   return ( 
     <div className = "App">
       <header className = "App-header">
       <h1>Login</h1> 
       <br /> 
-      <form action="/" method="GET"> {/* send to backend */}
+      <form onSubmit={formSubmit}> {/* send to backend */}
             <p>Username:</p>
-            <input type="text"/>
-        </form>
-        <br />
-        <form action="/" method="GET"> {/* send to backend */}
+            <input type="text" value={username} onChange={handleChangeUser}/>
             <p>Password:</p>
-            <input type="text"/>
+            <input type="text" value={password} onChange={handleChangePass}/>
+            <input type="submit" value="Submit"/>
         </form>
       <u>
         <p>amplifyb36@gmail.com</p>
