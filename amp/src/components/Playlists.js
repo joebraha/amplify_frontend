8e906edc536a2334389963e7ca81d32b95f79a35
emmaenglishwilkins <78217522+emmaenglishwilkins@users.noreply.github.com>
2023-11-28 15:45:04 -0500

    playlists page updated - not yet tested

--- amp/src/components/Playlists.js
+++ amp/src/components/Playlists.js
@@ -1,27 +1,95 @@
-import React, { useState } from "react"; 
-// importing Link from react-router-dom to navigate to  
-// different end points. 
-import { Link } from "react-router-dom"; 
-import { useEffect } from "react";
-  
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
+// Import audio files
+import audio1 from "../audios/audio1.wav";
+import audio2 from "../audios/audio2.wav";
+import audio3 from "../audios/audio3.wav";
+import audio4 from "../audios/audio4.wav";
 
-//TODO: Fill with saved playlists
-const Playlists = () => { 
+const Playlists = () => {
+  const [buttonNames, setButtonNames] = useState(["Play", "Play", "Play", "Play"]);
+  const audios = [audio1, audio2, audio3, audio4];
+  const audioRefs = new Array(4).fill(null);
 
+  const handleButtonClick = (index) => {
+    const newButtonNames = [...buttonNames];
+    if (newButtonNames[index] === "Play") {
+      audioRefs[index].play();
+      newButtonNames[index] = "Pause";
+    } else {
+      audioRefs[index].pause();
+      newButtonNames[index] = "Play";
+    }
+    setButtonNames(newButtonNames);
+  };
 
-  return ( 
-    <div> 
-      <h1>Saved Playlists</h1> 
-      <br /> 
-      <ul>
-      </ul>
-      <li> 
-          {/* Endpoint to route to Home Page */} 
-          <Link to="/">Return to Home</Link> {}
+  const handleFileChange = (e, index) => {
+    if (e.target.files[0]) {
+      const newAudios = [...audios];
+      newAudios[index] = URL.createObjectURL(e.target.files[0]);
+      setAudios(newAudios);
+    }
+  };
+
+  useEffect(() => {
+    audios.forEach((audio, index) => {
+      if (audio) {
+        audioRefs[index] = new Audio(audio);
+        audioRefs[index].onended = () => {
+          const newButtonNames = [...buttonNames];
+          newButtonNames[index] = "Play";
+          setButtonNames(newButtonNames);
+        };
+      }
+    });
+    // Cleanup function to release resources when component unmounts
+    return () => {
+      audioRefs.forEach((audioRef) => {
+        if (audioRef) {
+          audioRef.pause();
+          audioRef.src = "";
+          audioRef.load();
+        }
+      });
+    };
+  }, [audios, buttonNames]);
+
+  return (
+    <div>
+      <h1>Saved Playlists - Audio Player</h1>
+      {[...Array(5)].map((_, index) => (
+        <div key={index}>
+          <button onClick={() => handleButtonClick(index)}>{buttonNames[index]}</button>
+          <input type="file" onChange={(e) => handleFileChange(e, index)} />
+        </div>
+      ))}
+      <li>
+        <Link to="/">Return to Home</Link>
       </li>
-    </div> 
-  ); 
-}; 
+    </div>
+  );
+};
+
+export default Playlists;
+
+
+// //TODO: Fill with saved playlists
+// const Playlists = () => { 
+
+
+//   return ( 
+//     <div> 
+//       <h1>Saved Playlists</h1> 
+//       <br /> 
+//       <ul>
+//       </ul>
+//       <li> 
+//           {/* Endpoint to route to Home Page */} 
+//           <Link to="/">Return to Home</Link> {}
+//       </li>
+//     </div> 
+//   ); 
+// }; 
   
-export default Playlists; 
+// export default Playlists; 
