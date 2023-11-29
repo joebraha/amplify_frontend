80555988bf9bfaaa9cf72a80931d75b3116073b7
emmaenglishwilkins <78217522+emmaenglishwilkins@users.noreply.github.com>
2023-11-29 11:58:30 -0500

    get playlist - from spotify -

--- amp/src/components/GetPlaylists.js
+++ amp/src/components/GetPlaylists.js
@@ -1,46 +1,3 @@
-// // npm install axios 
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-
-// const PLAYLISTS_ENDPOINT = "https://api.spotify.com/v1/me/playlists";
-
-// const SpotifyGetPlaylists = () => {
-//     // state variables to store data that we get from API call (spotify)
-//     const [token, setToken] = useState("");
-//     const [data, setData] = useState({});
-
-//     // get token this is for stream lining the connection to spotify process
-//     useEffect(() => {
-//         if (localStorage.getItem("accessToken")) {
-//         setToken(localStorage.getItem("accessToken"));
-//         }
-//     }, []);
-
-//     const handleGetPlaylists = () => {
-//         axios
-//         .get(PLAYLISTS_ENDPOINT, {
-//             headers: {
-//             Authorization: "Bearer " + token,
-//             },
-//         })
-//         .then((response) => {
-//             setData(response.data);
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//         });
-//     };
-
-//     return (
-//         <>
-//         <button onClick={handleGetPlaylists}>Get Playlists</button>
-//         {data?.items ? data.items.map((item) => <p>{item.name}</p>) : null}
-//         </>
-//     );
-// };
-
-// export default SpotifyGetPlaylists;
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -108,7 +65,7 @@ const SpotifyGetPlaylists = () => {
                         <p>Album: {track.track.album.name}</p>
                         <p>Main Artist: {track.track.artists[0].name}</p>
                         <p>Artist Popularity: {track.track.artists[0].popularity}</p>
-                        <p>Genres: {track.track.artists[0].genres.join(", ")}</p>
+                        <p>Genres: {track.track.artists[0]?.genres ? track.track.artists[0].genres.join(", ") : 'N/A'}</p>
                     </li>
                 ))}
             </ul>
