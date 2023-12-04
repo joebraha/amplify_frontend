8ac185ce7dd2a2da81355016addf2684c535b0be
emmaenglishwilkins <78217522+emmaenglishwilkins@users.noreply.github.com>
2023-12-04 00:06:25 -0500

    spotify key to backend

--- amp/src/components/LoginSpotify.js
+++ amp/src/components/LoginSpotify.js
@@ -3,10 +3,10 @@ import GetPlaylists from "./GetPlaylists";
 import '../App.css';
 import { Link } from "react-router-dom"; 
 
-const CLIENT_ID = "c0e9c73676684f3e8d10acc56b94be60" // specific to my spotify application ? 
-const REDIR_URI = "http://localhost:3000/spotify" 
-const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-const RESPONSE_TYPE = "token"
+const CLIENT_ID = "c0e9c73676684f3e8d10acc56b94be60";
+const REDIR_URI = "http://localhost:3000/spotify";
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+const RESPONSE_TYPE = "token";
 const SPACE_DELIMITER = "%20";
 const SCOPES = [
   "user-read-currently-playing",
@@ -18,11 +18,10 @@ const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
 const getReturnedParamsFromSpotifyAuth = (hash) => {
   const stringAfterHashtag = hash.substring(1);
   const paramsInUrl = stringAfterHashtag.split("&");
-  const paramsSplitUp = paramsInUrl.reduce((accumulater, currentValue) => {
-    console.log(currentValue);
+  const paramsSplitUp = paramsInUrl.reduce((accumulator, currentValue) => {
     const [key, value] = currentValue.split("=");
-    accumulater[key] = value;
-    return accumulater;
+    accumulator[key] = value;
+    return accumulator;
   }, {});
 
   return paramsSplitUp;
@@ -34,28 +33,41 @@ const LoginSpotify = () => {
       const { access_token, expires_in, token_type } =
         getReturnedParamsFromSpotifyAuth(window.location.hash);
 
-      localStorage.clear();
-
-      localStorage.setItem("accessToken", access_token);
-      localStorage.setItem("tokenType", token_type);
-      localStorage.setItem("expiresIn", expires_in);
+      // Send the token to your backend here
+      sendTokenToBackend(access_token);
     }
-  });
+  }, []);
+
+  const sendTokenToBackend = (token) => {
+    // Send a POST request to your backend endpoint /api/login
+    fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ token }), // Sending the token in the request body
+    })
+    .then(response => {
+      // Handle the response from the backend if needed
+      console.log(response);
+    })
+    .catch(error => {
+      // Handle any errors that occur during the fetch operation
+      console.error('Error:', error);
+    });
+  };
 
   const handleLogin = () => {
     window.location = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIR_URI}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
   };
 
-    return (
+  return (
     <div className="App">
       <header className="App-header">
         <h1>Spotify Login</h1>
-        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIR_URI}&response_type=${RESPONSE_TYPE}`}>Login
-                  to Spotify</a>
-    
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIR_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
         {/* Endpoint to route to Home Page */} 
-        <Link to="/">Return to Home</Link> {}
-        
+        <Link to="/">Return to Home</Link>
         <GetPlaylists />
       </header>
     </div>
