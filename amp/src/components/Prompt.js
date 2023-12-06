774fa369acc723c46f101c70dda070f17ed4b10f
emmaenglishwilkins <78217522+emmaenglishwilkins@users.noreply.github.com>
2023-12-06 14:52:21 -0500

    change to prompt to go with file type from backend

--- amp/src/components/Prompt.js
+++ amp/src/components/Prompt.js
@@ -4,37 +4,29 @@ import '../App.css';
 
 const Prompt = () => {
   const [promptText, setPromptText] = useState("");
-  // const [fileUrl, setFileUrl] = useState(null);
-  // test fileUrl
-  const [fileUrl, setFileUrl] = useState("/Users/emmawilkins/Desktop/amplify_frontend/amp/src/Player/audios/audio1.wav");
+  const [fileUrl, setFileUrl] = useState(null);
 
   const normalize = text => {
     return text.replace(/ /g, '-');
-
   };
 
   const handlePromptSubmit = async (event) => {
     event.preventDefault();
 
-    const apiUrl = `http://localhost:8000/generate/${normalize(promptText)}`; // api url from backend goes here (then backend gets from Colab, saves it, and send it back here)
+    const apiUrl = `http://localhost:8000/generate/${normalize(promptText)}`;
 
-    // formData = new FormData()
-    // formData.append('input', promptText)
-
-    // Make a request to the backend
-    await fetch(apiUrl + normalize(promptText))
-      .then(async (response) => {
-        if (response.ok) {
-          const blob = await response.blob(); // Get the response as a blob
-          const url = URL.createObjectURL(blob); // Create a URL for the blob
-          setFileUrl(url); // Set the URL in state to display the file
-        } else {
-          console.error("Error generating file:", response.statusText);
-        }
-      })
-      .catch(error => {
-        console.error("Error fetching data:", error);
-      });
+    try {
+      const response = await fetch(apiUrl);
+      if (response.ok) {
+        const blob = await response.blob();
+        const url = URL.createObjectURL(blob);
+        setFileUrl(url);
+      } else {
+        console.error("Error generating file:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
 
   const handleInputChange = (event) => {
@@ -55,7 +47,7 @@ const Prompt = () => {
           <div>
             <audio controls>
               <source src={fileUrl} type="audio/wav" />
-              browser does not support the audio element.
+              Your browser does not support the audio element.
             </audio>
             <p>Download your generated file:</p>
             <a href={fileUrl} download="generated_file.wav">Download File</a>
